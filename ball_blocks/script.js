9b16dcbc16bbf2445121d6a49516eb5142f89828
krbrennan <krb.brennan@gmail.com>
2018-08-01 15:33:57 -0400

    canvas and ball physics done

--- ball_blocks/script.js
+++ ball_blocks/script.js
@@ -1,12 +1,14 @@
 canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
 
-
 const fps = 30
 setInterval(updateAll, 1000 / fps)
 
-let ballX = 100;
-let ballY = 100;
+let ballX = 600;
+let ballY = 600;
+
+let ballSpeedX = 5;
+let ballSpeedY = 5;
 
 let paddleX = canvas.width / 2;
 let paddleY = canvas.height - 10;
@@ -14,8 +16,22 @@ const paddleThickness = 10;
 const paddleLength = 60;
 
 function updateAll(){
-  ballX++;
-  ballY++;
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+// update ball trajectory if it exceeds bounds of the canvas
+  if(ballX <= 0){
+    ballSpeedX *= -1;
+  }
+  if(ballX >= canvas.width){
+    ballSpeedX *= -1;
+  }
+  if(ballY <= 0){
+    ballSpeedY *= -1;
+  }
+  if(ballY >= canvas.height){
+    ballSpeedY *= -1;
+  }
+
   // background
   ctx.fillStyle = 'black';
   ctx.fillRect(0,0, canvas.width, canvas.height);
@@ -25,6 +41,7 @@ function updateAll(){
   ctx.beginPath();
   ctx.arc(ballX, ballY, 10, 0, Math.PI * 2, true);
   ctx.fill();
+  // handleBall();
 
   // paddle
   ctx.fillStyle = 'white';
@@ -32,8 +49,20 @@ function updateAll(){
 }
 
 function handlePaddle(e){
-  console.log(e)
+  paddleX = e.offsetX - (paddleLength / 2);
 }
 
-// canvas.addEventListener('mouseover', handlePaddle);
+// function handleBall(e){
+//   ctx.fillStyle = 'white';
+//   ctx.beginPath();
+//   ctx.arc(ballX, ballY, 10, 0, Math.PI * 2, true);
+//   ctx.fill();
+//
+// }
+
+// function showXY(e){
+//   console.log(e)
+// }
+
 canvas.addEventListener('mousemove', handlePaddle);
+// canvas.addEventListener('mousemove', showXY);
